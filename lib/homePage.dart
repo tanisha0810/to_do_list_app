60cc16f943320fb02edd6c9b1d0b45c35b43bb42
Tanisha Varshney <tanishavarshney1998@gmail.com>
2020-11-19 00:12:51 +0530

    Added Shared Preferences

--- lib/homePage.dart
+++ lib/homePage.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class HomePage extends StatefulWidget {
   @override
@@ -6,8 +7,41 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  //List to store entries of To Do List
   List<String> task = [];
+
+  //Shared Preferences to store data on local device
+  SharedPreferences sharedPreferences;
+
+  //Text Field to take entry from user
   TextEditingController _textController = TextEditingController();
+
+  @override
+  void initState() {
+    loadSharedPreferences();
+    super.initState();
+  }
+
+  void loadSharedPreferences() async {
+    sharedPreferences = await SharedPreferences.getInstance();
+    loadData();
+  }
+
+  void saveData() {
+    //Saving Data in shared preferences
+    List<String> saveList = task;
+    sharedPreferences.setStringList('list', saveList);
+  }
+
+  void loadData() {
+    //Retrieving Data from shared preferences
+    List<String> loadList = sharedPreferences.getStringList('list');
+    if (loadList != null) {
+      setState(() {
+        task = loadList;
+      });
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -16,6 +50,8 @@ class _HomePageState extends State<HomePage> {
         title: Text('My To Do List'),
       ),
       body: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
         children: [
           Image.asset('assets/bg.png'),
           Expanded(
@@ -23,44 +59,79 @@ class _HomePageState extends State<HomePage> {
               padding: EdgeInsets.all(16.0),
               color: Colors.yellow[100],
               child: ListView.separated(
-                  itemBuilder: (BuildContext context, int index) {
-                    return Dismissible(
-                      key: Key(task[index]),
-                      child: ListTile(
-                        tileColor: Colors.orange[200 + index % 6 * 100],
-                        title: Text('${task[index]}'),
-                      ),
-                      onDismissed: (direction) {
-                        setState(() {
+                itemBuilder: (BuildContext context, int index) {
+                  return Dismissible(
+                    //To remove entries from To Do List by swiping action
+                    key: Key(task[index]),
+                    child: ListTile(
+                      tileColor: Colors.orange[200 + index % 6 * 100],
+                      title: Text('${task[index]}'),
+                    ),
+                    onDismissed: (direction) {
+                      setState(
+                        () {
                           task.removeAt(index);
-                        });
-                      },
-                    );
-                  },
-                  separatorBuilder: (BuildContext context, int index) =>
-                      Divider(),
-                  itemCount: task.length),
+                          saveData();
+                        },
+                      );
+                    },
+                  );
+                },
+                separatorBuilder: (BuildContext context, int index) =>
+                    Divider(),
+                itemCount: task.length,
+              ),
             ),
           ),
           Padding(
             padding: const EdgeInsets.all(16.0),
-            child: TextField(
-              controller: _textController,
-              decoration: InputDecoration(
-                hintText: 'Add New Task',
+            child: Container(
+              child: Row(
+                children: [
+                  Expanded(
+                    child: TextField(
+                      //To take user entry
+                      controller: _textController,
+                      decoration: InputDecoration(
+                        hintText: 'Add New Task',
+                      ),
+                      onSubmitted: (value) {
+                        if (value.length > 0) {
+                          setState(
+                            () {
+                              task.add(value);
+                              _textController.clear();
+                              saveData();
+                            },
+                          );
+                        }
+                      },
+                    ),
+                  ),
+                  SizedBox(
+                    width: 20,
+                  ),
+                  RaisedButton(
+                    onPressed: () {
+                      if (_textController.text.length > 0) {
+                        setState(
+                          () {
+                            task.add(_textController.text);
+                            _textController.clear();
+                            saveData();
+                          },
+                        );
+                      }
+                    },
+                    child: Text('ADD'),
+                    color: Colors.brown,
+                    textColor: Colors.white,
+                  )
+                ],
               ),
             ),
           ),
         ],
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          setState(() {
-            task.add(_textController.text);
-            _textController.clear();
-          });
-        },
-        child: Icon(Icons.add),
       ),
     );
   }
